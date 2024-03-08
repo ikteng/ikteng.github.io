f9052413e516449acf5bc9364f15932d6fc106e7
Ik Teng <154477174+ikteng@users.noreply.github.com>
2024-03-07 21:50:04 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+function openTab(tabId) {
+    // Hide all sections
+    document.querySelectorAll('.section').forEach(section => {
+        section.classList.remove('active');
+    });
+
+    // Show the selected section
+    const selectedSection = document.getElementById(tabId);
+    if (selectedSection) {
+        selectedSection.classList.add('active');
+        
+        // Update the URL to reflect the current section
+        history.pushState(null, null, `#${tabId}`);
+    }
+}
+
+function downloadResume() {
+    // Provide the correct path to your resume PDF file
+    const resumePath = "C:\\html_projects\\portfolio_website\\media\\Resume 3.pdf";
+    
+    // Create an anchor element
+    const link = document.createElement("a");
+    
+    // Set the download attribute and the file path
+    link.download = "Your_Resume.pdf";
+    link.href = resumePath;
+    
+    // Trigger a click on the anchor element
+    link.click();
+}
+
+// Open and close topbar
+function openHiddenMenu() {
+    document.getElementById("myHiddenMenu").style.display = "block";
+  }
+  
+  function closeHiddenMenu() {
+    document.getElementById("myHiddenMenu").style.display = "none";
+  }
