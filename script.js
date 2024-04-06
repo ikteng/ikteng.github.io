b5c522c5000ffdfe7bff23fb069c4d42334606bc
Ik Teng <154477174+ikteng@users.noreply.github.com>
2024-04-06 12:48:05 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -44,3 +44,14 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
+// Smooth scrolling to the target section
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
