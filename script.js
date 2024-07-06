d77f9e5438968c896cd6cc632cc7aa096da741f2
Ik Teng <iktengliaw@gmail.com>
2024-07-06 23:07:40 +0800

    Updated website

--- script.js
+++ script.js
@@ -22,39 +22,3 @@ function downloadResume() {
 function openProjectLink(projectLink) {
   window.open(projectLink, '_blank');
 }
-
-document.addEventListener("DOMContentLoaded", function() {
-  const sections = document.querySelectorAll(".section");
-
-  const fadeInOptions = {
-    threshold: 0.3 // Trigger the animation when at least 50% of the section is visible
-  };
-
-  const fadeInObserver = new IntersectionObserver(function(entries, observer) {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("visible");
-        observer.unobserve(entry.target);
-      }
-    });
-  }, fadeInOptions);
-
-  sections.forEach(section => {
-    fadeInObserver.observe(section);
-  });
-});
-
-// Smooth scrolling to the target section
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-});
-
-
-
-
